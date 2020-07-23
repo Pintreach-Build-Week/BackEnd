177d4ec07d3ba81d85810110f04090b3318cd092
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-07-22 17:56:58 -0700

    finished POST /register

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -22,3 +22,28 @@ router.get("/users/:id", async (req, res, next) => {
     next(err);
   }
 });
+
+//this POST endpoint will allow you to register a new user
+router.post("/register", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await Users.findBy({ username }).first();
+
+    //this checks if a username is already taken
+    if (user) {
+      return res.status(409).json({
+        message: "Username is already taken",
+      });
+    }
+
+    //this creates a new user while encrypting the password for security
+    const newUser = await Users.add({
+      username,
+      password: await bcrypt.hash(password, 14),
+    });
+
+    res.status(201).json(newUser);
+  } catch (err) {
+    next(err);
+  }
+});
