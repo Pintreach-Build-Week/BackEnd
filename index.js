fee28d1b5225093fec3ebe359be2629d654220b0
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-07-22 17:02:36 -0700

    formatting index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const server = require("./api/server") //imports server.js
+const server = require("./api/server"); //imports server.js
 
-const port = process.env.port || 4000
+const port = process.env.port || 4000;
 
 if (!module.parent) {
-    server.listen(port, () => {
-        console.log(`Server running at http://localhost: ${port}`)
-    })
+  server.listen(port, () => {
+    console.log(`Server running at http://localhost: ${port}`);
+  });
 }
