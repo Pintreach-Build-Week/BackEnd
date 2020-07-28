3e7b8b1c6b8fe94781bbe8000efccf0ed898d1a1
ron-hughes <ronald.hughes1982@gmail.com>
2020-07-28 11:53:47 -0400

    started adding routers

--- routes/article-model.js
+++ routes/article-model.js
@@ -1,1 +1,22 @@
-// model goes here
+const db = require('../data/dbConfig.js')
+
+module.exports = {
+    getArticles,
+    getArticle,
+    addArticle,
+    delArticle,
+}
+
+function getArticles  () {
+    return db('recipes')
+}
+
+function getArticle(id) {
+    return db("recipes").where({ id })
+ }
+ function addArticle(recipe) {
+    return db("recipes").insert(recipe)
+ }
+function delArticle(id) {
+    return db("recipes").where("id", id).del()
+}
--- routes/article-router.js
+++ routes/article-router.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const Articles = ("./article-model.js")
 
 // routers go here
 
+//GET ALL ARTICLES  
 
+router.get("/articles", (req, res) => {
+    Articles.getArticles()
+    .then(articles => {
+        res.status(200).json(articles)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message:
+              "An error occured while trying to get the recipes from the database.",
+            error: err
+        })
+    })
+})
 
 
 module.exports = router;
