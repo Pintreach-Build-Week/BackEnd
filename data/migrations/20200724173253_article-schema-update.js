2c97f4ede94cb0950b6c95fdc70ffb33d763061c
ron-hughes <ronald.hughes1982@gmail.com>
2020-07-30 09:53:14 -0400

    updated migrations

--- data/migrations/20200724173253_article-schema-update.js
+++ data/migrations/20200724173253_article-schema-update.js
@@ -1,12 +1,22 @@
 exports.up = async function (knex) {
-    await knex.schema.createTable("articles", (table) => {
-      table.increments(); //auto creates id
-      table.text("article").notNull().unique();
-    });
-  };
-  
-  exports.down = async function (knex) {
-    await knex.schema.dropTableIfExists("articles");
-    table.dropColumn("article");
-  };
-  
+  await knex.schema.createTable("articles", (table) => {
+    table.increments(); //auto creates id
+    table.text("title").notNull().unique();
+    table.text("url").notNull().unique();
+    table.text("author").notNull();
+    table.text("categories").notNull();
+    table.text("notes").notNull();
+  });
+};
+
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists("articles", (table) => {
+    table.dropColumn("title");
+    table.dropColumn("url");
+    table.dropColumn("author");
+    table.dropColumn("categories");
+    table.dropColumn("notes");
+
+  });
+
+};
